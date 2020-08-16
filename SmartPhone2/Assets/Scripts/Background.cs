09d42ddc72bada480e7188f9528487e44a4193af
EvandroFilho <epbf.snf17@uea.edu.br>
2020-08-16 18:53:21 -0400

    adicionando projeto smartphone 1 e 2

---
+++ SmartPhone2/Assets/Scripts/Background.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class Background : MonoBehaviour {
+    public float speed = 0.1f;
+    private Vector2 savedOffset;
+    new private Renderer renderer;
+
+    // Use this for initialization
+    void Start(){
+        renderer = GetComponent<Renderer>();
+        savedOffset = renderer.sharedMaterial.GetTextureOffset("_MainTex");
+    }
+
+    // Update is called once per frame
+    void Update(){
+        float x = Mathf.Repeat(Time.time * speed, 1);
+        Vector2 offset = new Vector2(x, savedOffset.y);
+        renderer.sharedMaterial.SetTextureOffset("_MainTex", offset);
+    }
+
+}
