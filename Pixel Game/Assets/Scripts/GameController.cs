fc7a9a7b20d4ac63984f7a30c0820fe43b2697a7
EvandroFilho <epbf.snf17@uea.edu.br>
2020-09-04 18:00:49 -0400

    Adicionando Sistema De Manter pontuação

--- Pixel Game/Assets/Scripts/GameController.cs
+++ Pixel Game/Assets/Scripts/GameController.cs
@@ -5,7 +5,9 @@ using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 public class GameController : MonoBehaviour
 {   
+    public int lastScore;
     public int totalScore;
+    public int sceneLoadedInt;
     public Text scoreText;
 
     public GameObject gameOver;
@@ -14,11 +16,33 @@ public class GameController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        //Pegar cena atual
+        Scene sceneLoaded = SceneManager.GetActiveScene();
+        sceneLoadedInt = sceneLoaded.buildIndex;
+        //Se for primeira cena, deletar score salvo 
+        if(sceneLoadedInt == 0){
+            PlayerPrefs.DeleteKey("LastScore");
+            if(PlayerPrefs.HasKey("LastScore")){
+                lastScore = PlayerPrefs.GetInt("LastScore");
+            }
+        //Senao tranferir score salvo pro score atual    
+        }else{
+            lastScore = PlayerPrefs.GetInt("LastScore");
+            totalScore = lastScore;
+            UpdateScoreText();
+        }
+
+
+        /* 
+        */
         instance = this;
     }
     
+    //Salvar score atual e mostrar na tela
     public void UpdateScoreText()
     {
+        PlayerPrefs.SetInt("LastScore", totalScore);
+        Debug.Log( PlayerPrefs.GetInt("LastScore"));
         scoreText.text = totalScore.ToString();
     }
 
